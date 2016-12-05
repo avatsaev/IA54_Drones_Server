ca47d6cfd47ea66212591d634943860164ad2983
A. Vatsaev <azero79@gmail.com>
2016-12-05 16:00:53 +0100

    implemented socketio server

---
+++ Drones/DroneAgent.js
@@ -0,0 +1,18 @@
+
+
+let uuid = require('uuid/v4');
+//id
+//init_pos
+//speed
+//current state
+
+class DroneAgent{
+
+  constructor(data= {}){
+    this.id = uuid()
+  }
+
+}
+
+
+module.exports = DroneAgent;
--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 let io = require('socket.io')(8080);
 
+let DroneAgent = require("./Drones/DroneAgent");
 
+let drones = [];
+
+let droneN = 10;
+
+for (let i = 0; i<droneN; i++){
+  drones.push(new DroneAgent())
+}
 
 io.on('connection', (socket) =>{
   console.log("connected");
@@ -8,6 +16,14 @@ io.on('connection', (socket) =>{
   socket.on('move', (data) =>{
     console.log(`onMove ${data.x} sdsd`);
     socket.emit("onMove", {"x_": 12});
+  });
+
+  socket.on("getDroneList", () =>{
+
+    console.log("get drone list");
+
+    socket.emit("onDroneList", drones);
+
   });
 
   socket.on('disconnect', (data) => {
